1b5090233ce13596887b98b5cb27d3db366ef159
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-06-16 21:55:55 -0400

    complete

--- fetcher.js
+++ fetcher.js
@@ -18,9 +18,7 @@ const fetcher = function(url, path) {
       }
     
     });
-    /* console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage. */
+   
   });
 };
 
